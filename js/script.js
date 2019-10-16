3674c5c2e53f3d67277f0082a5023bb986404010
aaliyah joseph <s_aaliyah.joseph@ousd.org>
2019-10-16 01:31:14 +0000

    I got a button to work and I successfully declared a function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $( "document" ).ready(function() {
 
+$("#button").click(function(){
+        let result=$(".input").val();
+        let checkVowelResult = checkVowel(result)
+        $(".output").text(checkVowelResult);
 
+});
 
+function checkVowel (str){
+    var firstLetter= str[0];
+    var vowel= ["a","e","i","o","u"];
+    if(vowel.includes(firstLetter)) {
+        return "Starts with vowel"
+    }
+        return "Starts with constanant"
+}
 });
