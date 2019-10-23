5cb71b9ca95bd2e45a8aa4b94fb51965b90d63e3
aaliyah joseph <s_aaliyah.joseph@ousd.org>
2019-10-23 01:03:58 +0000

    Created a function that changes the input to pig latin.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 $( "document" ).ready(function() {
-
-$("#button").click(function(){
-        let result=$(".input").val();
-        let checkVowelResult = checkVowel(result)
-        $(".output").text(checkVowelResult);
-
+    $("#button").click(function() {
+        var userInput= $(".input").val();
+        $(".output").text(userInput);
+        var pigLatinWord= pigLatinizer(userInput);
+        $(".output").text(pigLatinWord)
+    });
 });
+
+function pigLatinizer(userInput) {
+   let isVowel= checkVowel(userInput);
+
+   if (isVowel===false) {
+    return userInput.slice(1)+userInput[0]+"ay"
+   }
+    return userInput + "ay"
+}
 
 function checkVowel (str){
     var firstLetter= str[0];
     var vowel= ["a","e","i","o","u"];
-    if(vowel.includes(firstLetter)) {
-        return "Starts with vowel"
+    if (vowel.includes(firstLetter)) {
+        return true;
     }
-        return "Starts with constanant"
+        return false;
 }
-});
+
